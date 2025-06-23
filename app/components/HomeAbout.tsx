9e21b9a639c37a67e84d169481177726106f6246
Bailey Latimer <baileyl@beautyindustrygroup.com>
2025-06-22 20:51:56 -0700

    updated hero about section

--- app/components/HomeAbout.tsx
+++ app/components/HomeAbout.tsx
@@ -6,13 +6,19 @@ export default function HomeAbout({ description }: HomeAboutProps) {
   return (
     <div className="home-about py-20 mx-d flex justify-end">
       <div className="content">
-        <h2 className="text-indent">
+        <h2 className="text-indent relative">
+          <svg 
+            viewBox="0 0 33 33" 
+            xmlns="http://www.w3.org/2000/svg" 
+            role="presentation" 
+            className="blurb__svg absolute"
+            style={{ left: 0, top: '0.1em' }}
+          >
+            <path d="M0 0h33v33H0V0Z" fill="currentColor"></path>
+          </svg>
           {description || 
             "Passionate about diversity and artistic expression through movement. My journey in the world of dance began 15 years ago, and since then I have explored various styles, focusing especially on heels, where I find a unique connection with my femininity and inner strength."}
         </h2>
-        <svg viewBox="0 0 33 33" xmlns="http://www.w3.org/2000/svg" role="presentation" className="blurb__svg">
-          <path d="M0 0h33v33H0V0Z" fill="currentColor"></path>
-        </svg>
       </div>
     </div>
   );
